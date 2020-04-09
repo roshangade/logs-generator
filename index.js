3b344098f269f26871f090aecdbbb7c143c7da7b
Roshan Gade <roshangade@users.noreply.github.com>
2020-04-09 15:11:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+var i = 0
+
+var timer = setInterval(() => {
+    logger()
+}, 25)
+
+var stop = (i) => {
+    console.log('============================', i)
+    if (i == 2) {
+        clearInterval(timer)
+    }
+}
+
+var logger = () => {
+    for (var x = 0; x < 100; x++) {
+        console.log((new Date).toISOString(), "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", x, i)
+    }
+    stop(i++)
+}
